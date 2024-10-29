e372d6b9c78ec75178a6ed59974f14d4efae8f9e
Nick Carboni <n.carboni27@gmail.com>
2024-10-29 12:05:47 +0100

    feat: added navbar component

--- milestone2/src/app/layout.tsx
+++ milestone2/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "../components/navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <html lang="en">
+          <body className={geistMono.className}>
+            {" "}
+            //remember Inter is simply the font I chose.
+            <Navbar /> // This sets the navbar on every page
+            {children}
+          </body>
+        </html>
+
         {children}
       </body>
     </html>
---
+++ milestone2/src/components/navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    // replace everything in between the <header> & <header /> tags
+    // with your navbar code from your earlier milestones
+    // NOTE: we use "class" in HTML but React is quirky so we have to 
+    // change all the "class" to "className"
+    <header className={style.navbar} >
+      <ul className={style.navList}>
+        <li><Link href="/blogs">Blog</Link></li>
+        <li><Link href="/portfolio">Portfolio</Link></li>
+        <li>
+          <Link href="/">
+            <img className={style.homeImage} src="images/me.jpg" alt="home-page image" />
+          </Link>
+        </li>
+        <li><Link href="/resume">Resume</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </header>
+  );
+}
