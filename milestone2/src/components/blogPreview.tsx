77bdc2be1bf09d8fc6a77c9f9f3daab1f422659e
Nick Carboni <n.carboni27@gmail.com>
2024-11-08 07:26:47 +0100

    feat: added blog page, dynamically built using blogPreview components

--- milestone2/src/components/blogPreview.tsx
+++ milestone2/src/components/blogPreview.tsx
@@ -1,26 +1,30 @@
 import React from "react";
-import style from "/../../blogPreview.module.css";
+import style from "./blogPreview.module.css";
 import { Blog } from "@/app/static/blogData";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function BlogPreview(props: Blog) {
   return (
-    // replace everything between the <div> & </div> tags
-    // with your code from earlier milestones
-    <div>
-      <Link href={"/blogs/" + props.slug}>
-        <h2> {props.title} </h2>
+    <div className={style.blogCard}>
+      <Link href={`/blogs/${props.slug}`}>
+          <h2 className={style.blogTitle}>{props.title}</h2>
       </Link>
-      <div>
-        <Image
-          src={props.image}
-          alt={props.imageAlt}
-          width={500}
-          height={500}
-        ></Image>
-        <p>{props.description}</p>
-        <p>{props.date}</p>
+      <div className={style.blogContent}>
+        <div className={style.blogImage}>
+          <Image
+            src={props.image}
+            alt={props.imageAlt}
+            width={500}
+            height={300}
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+        <div className={style.blogInfo}>
+          <p className={style.blogDescription}>{props.description}</p>
+          <p className={style.blogDate}>{props.date}</p>
+        </div>
       </div>
     </div>
   );
